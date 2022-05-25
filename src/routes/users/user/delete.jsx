5b4d744d6859d296524cf9f74e3002e3ad10cc8a
Dave Hudson <dave@applification.net>
2022-05-25 19:16:56 +0100

    Styling

--- src/routes/users/user/delete.jsx
+++ src/routes/users/user/delete.jsx
@@ -13,17 +13,19 @@ export default function UserDelete() {
   const user = useRouteLoaderData("UserDetails");
 
   return (
-    <div>
-      <h1 className="text-2xl font-bold text-gray-900 truncate">Delete</h1>
-      <p>Are you sure you want to delete the user {user.name}?</p>
-      <hr className="p-2" />
-      <Form method="post">
-        <button type="submit" className="inline-flex items-center px-5 py-2 border border-transparent text-base font-medium rounded-full shadow-sm text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
-          {navigation.state === "idle" && 'Yes, delete!!'}
-          {navigation.state === "submitting" && 'Deleting...'}
-          {navigation.state === "loading" && "Deleted!"}          
-        </button>
-      </Form>
+    <div className="sm:flex-1 sm:min-w-0 sm:flex sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
+      <div className="2xl:block mt-2 min-w-0 flex-1">
+        <h1 className="text-2xl font-bold text-gray-900 truncate">Delete</h1>
+        <p>Are you sure you want to delete the user {user.name}?</p>
+        <hr className="p-2" />
+        <Form method="post">
+          <button type="submit" className="inline-flex items-center px-5 py-2 border border-transparent text-base font-medium rounded-full shadow-sm text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
+            {navigation.state === "idle" && 'Yes, delete!!'}
+            {navigation.state === "submitting" && 'Deleting...'}
+            {navigation.state === "loading" && "Deleted!"}          
+          </button>
+        </Form>
+      </div>
     </div>
   )
 }
